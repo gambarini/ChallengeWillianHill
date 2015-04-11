9f148cd1637733dbf335f62dd8e615119fde191a
Gustavo Gambarini <gambarini@gmail.com>
2015-04-11 17:43:41 +1000

    Customer Controller and git ignore

---
+++ ChallengeWillianHill/REST/CustomerControler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web.Http;
+
+namespace ChallengeWillianHill.REST
+{
+	[RoutePrefix("Customer")]
+	public class CustomerControler : ApiController
+	{
+		
+		public CustomerControler ()
+		{
+		}
+
+
+	}
+}
+
